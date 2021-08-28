a06d93761b21569e603f6865a3ab2af653e75caf
dv6230 <67547654+dv6230@users.noreply.github.com>
2021-08-29 00:37:35 +0800

    login function

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+# LoginView = auth_views.LoginView
+# LoginView.as_view(template_name='registration/login2.html')
+
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,6 +3,31 @@ from django.shortcuts import render
 
 from django.contrib.auth import authenticate, login
 
+from .form import LoginForm
+
 
 def dashboard(request):
     return render(request, 'account/dashboard.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd['username'], password=cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse(
+                        'Authenticate successfully'
+                    )
+                else:
+                    return HttpResponse('Disabled acccount')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+        return render(request, 'account/login.html', {'form': form})
