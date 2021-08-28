bdb2c79dde1cf7e3a825646866ed2d1cca02ee34
dv6230 <67547654+dv6230@users.noreply.github.com>
2021-08-28 23:27:14 +0800

    create

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(),name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from django.contrib.auth import authenticate, login
 
 
+def dashboard(request):
+    return render(request, 'account/dashboard.html')
