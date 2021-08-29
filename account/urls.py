7969d10544358d177e9d11766bf35870aa506ab4
dv6230 <67547654+dv6230@users.noreply.github.com>
2021-08-29 16:27:31 +0800

    main

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('', views.dashboard, name='dashboard'),
+    path('register/',views.register,name='register'),
 ]
