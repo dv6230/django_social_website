b35dc0da19740501c07c9835d11ff7d9fd877615
dv6230 <67547654+dv6230@users.noreply.github.com>
2021-08-31 16:25:50 +0800

    login function

--- account/form.py
+++ account/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -9,9 +10,27 @@ class LoginForm(forms.Form):
 
 
 class MyUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="密碼",
+        error_messages={
+            'required': '必要',
+        },
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label="密碼確認",
+        error_messages={
+            'required': '必要',
+        },
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    error_messages = {
+        'password_mismatch': "兩者密碼不相同",
+    }
+
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
         labels = {
             'username': '暱稱',
             'email': '電子郵件',
@@ -23,6 +42,8 @@ class MyUserCreationForm(UserCreationForm):
         error_messages = {
             '__all__': {
                 'required': '請輸入內容',
-                'invalid': '請檢查輸入內容是否正確'
+                'invalid': '請檢查輸入內容是否正確',
+                'password_mismatch': "test content",
             },
+            'password_mismatch': ("test content"),
         }
