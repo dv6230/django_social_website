7d994f6279de4f51e46b8a7f51f93213f36ab59b
dv6230 <67547654+dv6230@users.noreply.github.com>
2021-09-09 12:15:56 +0800

    用戶追縱

--- account/models.py
+++ account/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
+from django.utils import timezone
+
 
 class Contract(models.Model):
     user_from = models.ForeignKey(
@@ -17,10 +18,21 @@ class Contract(models.Model):
         on_delete=models.CASCADE
     )
 
-    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    created = models.DateTimeField(db_index=True, default=timezone.now)
 
     class Meta:
         ordering = ('-created',)
 
     def __str__(self):
         return '{} follows {}'.format(self.user_from, self.user_to)
+
+
+User.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contract,
+        related_name='followers',
+        symmetrical=False,  # 非對稱結構
+    )
+)
